99c59ae1ac79cb9dc840f8de88ae12d730e61bf4
Glenn <2738328919@qq.com>
2023-03-08 02:07:28 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 // import logo from './logo.svg';
 // import "./App.css";
+import { Center} from "@chakra-ui/react";
 import Form from "./component/Form/Form";
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Center h="100vh">
+       
+        <Form />
+      </Center>
     </div>
   );
 }
--- src/component/Form/Form.tsx
+++ src/component/Form/Form.tsx
@@ -1,12 +1,12 @@
 // import { useFormik } from "formik";
 // 1.inout build, post request use, debugger
-import { Box, Input, Button } from "@chakra-ui/react";
+import { HStack, Box, Input, Button, Flex, Text } from "@chakra-ui/react";
 // import { StarIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 import styles from "./form.module.css";
 import axios from "axios";
 export default function Form() {
-  console.log(process.env.REACT_APP_API)
+  console.log(process.env.REACT_APP_API);
   // let server: string = process.env.REACT_APP_API;
   const [longUrl, setLongUrl] = useState("");
   const [shortId, setShortId] = useState("");
@@ -28,24 +28,44 @@ export default function Form() {
     }
   };
   return (
-    <Box>
-      <h2>Generate a short URL</h2>
-      <form onSubmit={handleSubmit}>
-        <Input
-          value={longUrl}
-          onChange={handleChange}
-          placeholder="Here is a sample placeholder"
-          size="lg"
-          className={styles["chakra-input css-6p9lc8"]}
-        />
-        <Button type="submit">Generate Short Url</Button>
-      </form>
+    <div>
+      <Flex align="center" justify="center" height="200px">
+        <Text fontSize="2xl" textAlign="right" fontWeight="bold" mb="auto">
+          Generate a short URL
+        </Text>
+      </Flex>
+      <Box m="3">
+        <form onSubmit={handleSubmit}>
+          <HStack spacing={4}>
+            <Input
+              value={longUrl}
+              onChange={handleChange}
+              placeholder="Shotern your link"
+              size="lg"
+              className={styles["chakra-input css-6p9lc8"]}
+              width="500px"
+            />
+            <Button type="submit" colorScheme={"blue"}>
+              Shorten
+            </Button>
+          </HStack>
+        </form>
+      </Box>
       {/* only show the short url when there is a short id */}
       {shortId && (
-        <a href={`${process.env.REACT_APP_API}/${shortId}`}>
-          {process.env.REACT_APP_API}/{shortId ? shortId : ""}
-        </a>
+        <Box  height="42px" bg="white" borderRadius="md" m="3">
+          <HStack spacing={1}>
+            <Text textDecoration="underline" width="500px">
+              <a href={`${process.env.REACT_APP_API}/${shortId}`}>
+                {process.env.REACT_APP_API}/{shortId ? shortId : ""}
+              </a>
+            </Text>
+            <Button type="submit" colorScheme={"green"}>
+              shorted link
+            </Button>
+          </HStack>
+        </Box>
       )}
-    </Box>
+    </div>
   );
 }
