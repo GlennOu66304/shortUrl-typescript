6c506dd56b06a221bbe6a83d0b541e75f8c2c79f
Glenn <2738328919@qq.com>
2023-03-22 00:03:54 +0400

    update short url list

--- src/component/qrCard/QrCard.tsx
+++ src/component/qrCard/QrCard.tsx
@@ -2,25 +2,30 @@ import React from "react";
 // import styles from "./QrCard.module.css";
 // put the  details and edit in a row
 import { Flex, Box, Stack, Button } from "@chakra-ui/react";
-export const QrCard: React.FC = () => {
+interface itemProps {
+  name2: string;
+  longurl2: string;
+}
+export const QrCard: React.FC <itemProps >= ({name2,longurl2}) => {
+  // console.log(item2);
   return (
     <div className="QrCard">
       {/* qr code details */}
       <Flex>
         <Box flex="1" className="details">
-          <p>Name:</p>
-          <p>long url:</p>
-          <p>short url:</p>
+          <p>Name:{name2}</p>
+          <p>long url:{longurl2}</p>
+          <p>short url:{`${process.env.REACT_APP_API}/`}{name2}</p>
         </Box>
 
-        <Box flex="1" className="image">
+        {/* <Box flex="1" className="image">
           <img
             src={
               "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
             }
             alt="qr code "
           />
-        </Box>
+        </Box> */}
 
         <Box flex="1" className="edit">
           <Stack direction="row" spacing={2}>
--- src/component/qrCards/QrCards.tsx
+++ src/component/qrCards/QrCards.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 // import styles from "./QrCards.module.css";
-
 
 import { QrCard } from "../qrCard/QrCard";
 export const QrCards: React.FC = () => {
-  // const [data, setData] = useState([1, 2, 3]);
-  const data = [1, 2, 3]
+  const [shortUrlList, setShortUrlList] = useState([]);
+  useEffect(() => {
+    const loadShortUrlList = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API}/api/list`);
+      // console.log(result.data);
+      setShortUrlList(result.data);
+    };
+    loadShortUrlList();
+  }, []);
+
   return (
     <div className="QrCards">
       {/* <QrCard /> */}
       <div>
-        {data.map((item, index) => {
-          return <QrCard key={index} />;
+        {shortUrlList.map((item, index) => {
+          return <QrCard name2={item.shortId}  longurl2={item.longUrl} key={index} />;
         })}
       </div>
     </div>
