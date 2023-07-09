48dca4347801f54677b5048906485e17ecd87b48
Glenn Ou <[glennou2@gmail.com]>
2023-07-09 14:13:58 +0400

    update the persiste impport path issue

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { persistor } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/es/integration/react';
+// https://github.com/rt2zz/redux-persist/issues/1024
 import store from "./store";
 
 import { ChakraProvider } from "@chakra-ui/react";
